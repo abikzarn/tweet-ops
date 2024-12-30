4caa5a95512e50f2a6ce3affc475d89c000e8c42
Abdessalam <ab.ikzarn@gmail.com>
2024-12-30 23:11:23 +0100

    TF: 3 EC2, subnet, IG, RT

--- terraform_code/V2_EC2.tf
+++ terraform_code/V2_EC2.tf
@@ -6,21 +6,22 @@ provider "aws" {
 
 # Create EC2 instance
 resource "aws_instance" "demo-server" {
-  ami             = "ami-01816d07b1128cd2d"
-  instance_type   = "t2.micro"
-  key_name        = "mine"
-  subnet_id       = "subnet-066148bf06403b80b"
+  ami                    = "ami-005fc0f236362e99f"
+  instance_type          = "t2.micro"
+  key_name               = "mine"
+  subnet_id              = aws_subnet.dpw-public_subnet_01.id
   vpc_security_group_ids = [aws_security_group.demo-sg.id]
-  tags = {
-    Name = "demo-server"
-  }
-
+  for_each = toset(["jenkins-master", "build-slave", "ansible"])
+   tags = {
+     Name = "${each.key}"
+   }
 }
 
 # Create security group
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
   description = "SSH Access"
+  vpc_id      = aws_vpc.dpw-vpc.id
 
   ingress {
     description = "SSH Access"
@@ -39,4 +40,48 @@ resource "aws_security_group" "demo-sg" {
   tags = {
     Name = "ssh"
   }
+}
+
+resource "aws_vpc" "dpw-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "dpw-vpc"
+  }
+}
+
+resource "aws_subnet" "dpw-public_subnet_01" {
+  vpc_id                  = aws_vpc.dpw-vpc.id
+  cidr_block              = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "dpw-public_subnet_01"
+  }
+}
+
+//Creating an Internet Gateway 
+resource "aws_internet_gateway" "dpw-igw" {
+  vpc_id = aws_vpc.dpw-vpc.id
+  tags = {
+    Name = "dpw-igw"
+  }
+}
+
+// Create a route table 
+resource "aws_route_table" "dpw-public-rt" {
+  vpc_id = aws_vpc.dpw-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dpw-igw.id
+  }
+  tags = {
+    Name = "dpw-public-rt"
+  }
+}
+
+// Associate subnet with route table
+
+resource "aws_route_table_association" "dpw-rta-public-subnet-1" {
+  subnet_id      = aws_subnet.dpw-public_subnet_01.id
+  route_table_id = aws_route_table.dpw-public-rt.id
 }
